70e2d0fd32bb8ecc3d4bdd140493f2ea878027ef
suhas52 <suhasedu991@gmail.com>
2025-09-19 11:35:08 +0530

    rps logic complete

--- projects/rps/script.js
+++ projects/rps/script.js
@@ -1,25 +1,102 @@
 const userSection = document.querySelector(".user-section");
-const cpuSection = document.querySelector(".cpu-section")
-const cpuImg = document.getElementById("cpu-img")
-const choices = ["rock", "paper", "scizzors"]
-
-userSection.addEventListener("click", function (e) {
-  user_choice = e.target.id
-  console.log(user_choice)
-});
-
-function genCpuImg(cpuImg) {
-    let choice = choices[Math.floor(Math.random() * choices.length)];
-    if (choice === "rock") {
-        cpuImg.src = "images/rock.png"
-    }
-    if (choice === "paper") {
-        cpuImg.src = "images/paper.png"
-    }
-    if (choice === "scizzors") {
-        cpuImg.src = "images/scizzors.png"
-    }
-
+const midSection = document.querySelector(".mid-section");
+const cpuSection = document.querySelector(".cpu-section");
+const cpuImg = document.getElementById("cpu-img");
+const userScore = document.querySelector(".user_score");
+const cpuScore = document.querySelector(".cpu_score");
+const choices = ["rock", "paper", "scizzors"];
+const currentStatus = document.querySelector(".current-status");
+const retrybtn = document.createElement("button");
+retrybtn.textContent = "Retry?";
+let cpu_choice = genCpuImg();
+const rules = {
+    rock: "paper",
+    paper: "scizzors",
+    scizzors: "rock"
 };
 
-console.log(genCpuImg(cpuImg))
+let userScoreValue = 0;
+let cpuScoreValue = 0;
+
+function genCpuImg() {
+    let choice = choices[Math.floor(Math.random() * choices.length)];
+    if (choice === "rock") {
+        cpuImg.src = "images/rock.png";
+    }
+    if (choice === "paper") {
+        cpuImg.src = "images/paper.png";
+    }
+    if (choice === "scizzors") {
+        cpuImg.src = "images/scizzors.png";
+    }
+    return choice
+};
+
+function checkResult(user_choice, cpu_choice) {
+    if (user_choice == cpu_choice) {
+        return "draw";
+    }
+    else if (cpu_choice == rules[user_choice]) {
+        return "loss";
+    }
+    else {
+        return "win";
+    }
+}
+
+
+function handleUserClick(e) {
+    let user_choice = e.target.id;
+    let scoreCheck = checkResult(user_choice, cpu_choice);
+    if (scoreCheck == "win") {
+        userScoreValue += 1;
+        userScore.textContent = userScoreValue;
+        currentStatus.textContent = "You got a point!";
+    }
+    else if (scoreCheck == "loss") {
+        cpuScoreValue += 1;
+        cpuScore.textContent = cpuScoreValue;
+        currentStatus.textContent = "Your opponent got a point!";
+    }
+    else {
+        currentStatus.textContent = "That was a draw!"
+    }
+
+    if (userScoreValue === 5 || cpuScoreValue === 5) {
+        currentStatus.textContent = "Game Over!";
+        midSection.append(retrybtn)
+        userSection.removeEventListener("click", handleUserClick);
+    }
+    cpu_choice = genCpuImg();
+    cpuImg.removeAttribute("hidden")
+}
+
+userSection.addEventListener("click", handleUserClick);
+retrybtn.addEventListener("click", () => {
+    location.reload()
+});
+
+// userSection.addEventListener("click", function (e) {
+//     let user_choice = e.target.id;
+//     let scoreCheck = checkResult(user_choice, cpu_choice);
+//     if (scoreCheck == "win") {
+//         userScoreValue += 1;
+//         userScore.textContent = userScoreValue;
+//         currentStatus.textContent = "You got a point!";
+//     }
+//     else if (scoreCheck == "loss") {
+//         cpuScoreValue += 1;
+//         cpuScore.textContent = cpuScoreValue;
+//         currentStatus.textContent = "Your opponent got a point!";
+//     }
+//     else {
+//         currentStatus.textContent = "That was a draw!"
+//     }
+//     cpu_choice = genCpuImg();
+// });
+
+
+console.log(userSection)
+
+
+
