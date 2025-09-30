99cf7b185063941d911f8753b915f49d1723e7ca
suhas <suhasedu991@gmail.com>
2025-09-30 10:51:14 +0530

    rewrote the api snippet with await/async to learn those keywords

--- snippets/api/script.js
+++ snippets/api/script.js
@@ -2,19 +2,21 @@ import { API_KEY } from "./api.js";
 const img = document.querySelector('img');
 const URL = "https://api.giphy.com/v1/gifs/translate";
 const search = document.getElementById("search")
-let querry = "";
+let query = "";
 
 const moreCatsBtn = document.getElementById("more-cats");
 
-function getMoreCats(querry) {
-    return fetch(`${URL}?api_key=${API_KEY}&s=${querry}`)
-    .then(response => response.json())
-    .then(data => data.data.images.original.url);
+async function getMoreGifs(query) {
+    const response = await fetch(`${URL}?api_key=${API_KEY}&s=${query}`);
+    const data = await response.json();
+    return data.data.images.original.url;
 }
+
+
 moreCatsBtn.addEventListener("click", () => {
-    querry = search.value;
+    query = search.value;
     
-    getMoreCats(querry).then(url => {
+    getMoreGifs(query).then(url => {
         img.src = url;
         img.hidden = false;
     });
